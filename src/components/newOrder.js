b29dee9b75616f4ffc53a33333c226365a8c272e
manu293 <manojsm293@gmail.com>
2022-01-27 12:19:36 +0530

    Order styles

---
+++ src/components/newOrder.js
@@ -0,0 +1,296 @@
+// imports
+import React, { useState } from "react";
+
+// styles
+import "../styles/orders.css";
+
+const NewOrder = (props) => {
+    const {setAddOrder} = props;
+
+    const [currentActiveTab, setCurrentActiveTab] = useState(0);
+    const [currentRadioButton, setCurrentRadioButton] = useState("yarnGrn");
+
+    let currentSection;
+
+    const renderOrderDetailSection = () => {
+
+        return (
+            <>
+                <div className="accountReportTextField entryFilterMiddleSectionMargin">
+                    <input
+                        className="loginInSignUpCustomInput"
+                        type="text"
+                        id="documentDate"
+                        placeholder="Enter Document Date"
+                        value=""
+                    />
+                    <label className="loginSignUpLabel">Document Date</label>
+                </div>
+
+                <div className="accountReportTextField entryFilterMiddleSectionMargin">
+                    <input
+                        className="loginInSignUpCustomInput"
+                        type="text"
+                        id="customerName"
+                        placeholder="Enter Customer Name"
+                        value=""
+                    />
+                    <label className="loginSignUpLabel">Customer Name</label>
+                </div>
+
+                <div className="accountReportTextField entryFilterMiddleSectionMargin">
+                    <input
+                        className="loginInSignUpCustomInput"
+                        type="text"
+                        id="orderId"
+                        placeholder="Enter Order Id"
+                        value=""
+                    />
+                    <label className="loginSignUpLabel">Order Id</label>
+                </div>
+
+                <div className="accountReportTextField entryFilterMiddleSectionMargin">
+                    <input
+                        className="loginInSignUpCustomInput"
+                        type="text"
+                        id="referenceNumber"
+                        placeholder="Enter Reference Number"
+                        value=""
+                    />
+                    <label className="loginSignUpLabel">References</label>
+                </div>
+            </>
+        )
+
+    }
+
+    const renderFabricDetailSection = () => {
+        return (
+            <>
+                <div className="accountReportSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Fabric Type</label>
+                </div>
+
+                <div className="accountReportSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Gauge</label>
+                </div>
+
+                <div className="accountReportTextField entryFilterMiddleSectionMargin">
+                    <input
+                        className="loginInSignUpCustomInput"
+                        type="text"
+                        id="documentDate"
+                        placeholder="Enter Document Date"
+                        value=""
+                    />
+                    <label className="loginSignUpLabel">Loop Length</label>
+                </div>
+
+                <div className="accountReportTextField entryFilterMiddleSectionMargin">
+                    <input
+                        className="loginInSignUpCustomInput"
+                        type="text"
+                        id="documentDate"
+                        placeholder="Enter Document Date"
+                        value=""
+                    />
+                    <label className="loginSignUpLabel">GSM</label>
+                </div>
+
+                <div className="accountReportSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Knit Type</label>
+                </div>
+
+                <div className="accountReportSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Counts</label>
+                </div>
+
+                <div className="accountReportSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Mill</label>
+                </div>
+            </>
+        )
+    }
+
+    const renderGrnSection = () => {
+        return (
+            <>
+                <div className="renderGrnSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Counts</label>
+                </div>
+
+                <div className="renderGrnSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Mill</label>
+                </div>
+
+                <div className="renderGrnSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Type Of Yarn</label>
+                </div>
+
+                <div className="renderGrnSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Variety Of Yarn</label>
+                </div>
+
+                <div className="renderGrnSelectField entryFilterMiddleSectionMargin">
+                    <select className="loginInSignUpCustomInput">
+                        <option value="type1">Type 1</option>
+                        <option value="type2">Type 2</option>
+                    </select>
+                    <label className="loginSignUpLabel">Colour</label>
+                </div>
+
+            </>
+        )
+    }
+
+    switch(currentActiveTab) {
+        case 0:
+            currentSection = renderOrderDetailSection();
+            break;
+        case 1:
+            currentSection = renderFabricDetailSection();
+            break;
+        case 2:
+            currentSection = renderGrnSection();
+            break;
+        default:
+            currentSection = renderOrderDetailSection();
+            break;
+    }
+
+    return (
+        <div className="entryFilterContainer">
+            <div className="enteryFilterSection">
+
+                <div className="entryFilterMiddleHeader">
+
+                    <p  className={(currentActiveTab === 0) ? "entryFilterMiddleHeaderText entryFilterMiddleHeaderActiveText" : "entryFilterMiddleHeaderText"}
+                        onClick={() => setCurrentActiveTab(0)}
+                    >
+                        Order Details
+                    </p>
+                    <p  className={(currentActiveTab === 1) ? "entryFilterMiddleHeaderText entryFilterMiddleHeaderActiveText" : "entryFilterMiddleHeaderText"}
+                        onClick={() => setCurrentActiveTab(1)}
+                    >
+                        Fabric Details
+                    </p>
+                    {
+                        (currentRadioButton === "yarnGrn") &&
+                        (
+                            <p  className={(currentActiveTab === 2) ? "entryFilterMiddleHeaderText entryFilterMiddleHeaderActiveText" : "entryFilterMiddleHeaderText"}
+                                onClick={() => setCurrentActiveTab(2)}
+                            >
+                                Good Received Notes
+                            </p>
+                        )
+                    }
+
+                </div>
+
+                <div className="entryFilterCircleIndicatorContainer">
+                    <div className={(currentActiveTab === 0) ? "circleIndicatorActive" : "circleIndicator"} />
+                    <div className={(currentActiveTab === 1) ? "circleIndicatorActive" : "circleIndicator"} />
+                    {
+                        (currentRadioButton === "yarnGrn") &&
+                        (
+                            <div className={(currentActiveTab === 2) ? "circleIndicatorActive" : "circleIndicator"} />
+                        )
+                    }
+                    <div className="lineIndicator" />
+                </div>
+
+                <div className="entryFilterMiddleSection">
+                    
+                    {currentSection}
+
+                </div>
+
+                {
+                    (currentActiveTab === 0) &&
+                    (
+                        <div className="yarnAndFabricContainer">
+
+                            <div className="yarnContainer">
+                                <input
+                                    type="radio"
+                                    id="yarnGrn"
+                                    value="yarnGrn"
+                                    checked={(currentRadioButton === "yarnGrn") ? true : false}
+                                    onClick={() => setCurrentRadioButton("yarnGrn")}
+                                />
+                                <label className="yarnContainerLabel" htmlFor="yarnGrn">With Yarn GRN</label>
+                            </div>
+
+                            <div className="yarnContainer">
+                                <input
+                                    type="radio"
+                                    id="fabricDetail"
+                                    value="fabricDetail"
+                                    checked={(currentRadioButton === "fabricDetail") ? true : false}
+                                    onClick={() => setCurrentRadioButton("fabricDetail")}
+                                />
+                                <label className="yarnContainerLabel" htmlFor="fabricDetail">With Fabric Details</label>
+                            </div>
+
+                        </div>
+                    )
+                }
+
+                <div className="addNewOrderFooter">
+                    <button
+                        onClick={() => setAddOrder(false)}
+                        className="addNewOrderFooterCancel addNewOrderFooterMargin"
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        onClick={() => setAddOrder(false)}
+                        className="addNewOrderFooterSubmit"
+                    >
+                        Submit
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default NewOrder;
--- src/pages/orderEntry.js
+++ src/pages/orderEntry.js
@@ -1,12 +1,97 @@
 // imports
-import React from "react";
+import React, { useState } from "react";
+import {
+    Export,
+    FadersHorizontal,
+    CaretRight,
+    DotsThreeVertical,
+    Eye,
+    X
+} from "phosphor-react";
 
 // local imports
+import NewOrder from "../components/newOrder";
 import Navbar from "../components/navbar";
+import CustomHeader from "../components/customHeader";
 
-
+// styles
+import "../styles/orders.css";
 
 const OrderEntry = () => {
+
+    const [showFilterEntry, setFilterEntry] = useState(false);
+    const [showAddOrder, setAddOrder] = useState(false);
+    const [orderEntryFilter, setOrderEntryFilter] = useState({
+        date: "",
+        orderId: "",
+        customerName: "",
+    });
+
+    const handleOrderEntryFilterInput = (e) => {
+        const intermediateInput = {...orderEntryFilter};
+        intermediateInput[e.target.id] = e.target.value;
+        setOrderEntryFilter(intermediateInput);
+    }
+
+    const OrderEntryFilter = () => {
+        return (
+            <div className="entryFilterContainer">
+                <div className="enteryFilterSection">
+                    <div className="entryFilterHeaderSection">
+                        <X
+                            size={25}
+                            weight="bold"
+                            color="#FFA412"
+                            onClick={() => setFilterEntry(false)}
+                        />
+                    </div>
+
+                    <div className="entryFilterMiddleSection">
+
+                        <div className="accountReportTextField">
+                            <input
+                                className="loginInSignUpCustomInput"
+                                type="text"
+                                id="date"
+                                placeholder="Enter Date"
+                                value={orderEntryFilter.date}
+                                onChange={(e) => handleOrderEntryFilterInput(e)}
+                            />
+                            <label className="loginSignUpLabel">Date</label>
+                        </div>
+
+                        <div className="accountReportTextField">
+                            <input
+                                className="loginInSignUpCustomInput"
+                                type="text"
+                                id="orderId"
+                                placeholder="Enter Order Id"
+                                value={orderEntryFilter.orderId}
+                                onChange={(e) => handleOrderEntryFilterInput(e)}
+                            />
+                            <label className="loginSignUpLabel">Order Id</label>
+                        </div>
+
+                        <div className="accountReportTextField">
+                            <input
+                                className="loginInSignUpCustomInput"
+                                type="text"
+                                id="customerName"
+                                placeholder="Enter Customer Name"
+                                value={orderEntryFilter.customerName}
+                                onChange={(e) => handleOrderEntryFilterInput(e)}
+                            />
+                            <label className="loginSignUpLabel">Customer Name</label>
+                        </div>
+                    </div>
+
+                    <div className="entryFilterFooterSection">
+                        <button className="entryFilterFooterButton">Filter</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className="mainContainer">
@@ -14,7 +99,117 @@ const OrderEntry = () => {
             <Navbar />
 
             <div className="mainContentContainer">
+
+                <CustomHeader />
+
+                <div className="orderEntryHeader">
+
+                    <div className="orderEntryHeaderLeftSection">
+                        <p className="orderEntryHeaderSubText">Orders &nbsp; <CaretRight size={30} weight="bold" /> &nbsp; Order Entry</p>
+                        <div className="orderEntryHeaderIconContainer">
+                            <FadersHorizontal
+                                size={28}
+                                weight="bold"
+                                onClick={() => setFilterEntry(true)}
+                            />
+                            <Export size={28} weight="bold" />
+                        </div>
+                    </div>
+
+                    <div className="orderEntryRightSection">
+                        <button
+                            onClick={() => setAddOrder(true)}
+                            className="orderEntryHeaderButton"
+                        >
+                            Create
+                        </button>
+                    </div>
+
+                </div>
+
+                <div className="orderEntryMiddleContainer">
+                    <table className="orderEntryTable">
+                        <thead>
+                            <tr className="orderEntryTableHeader">
+                                <th className="orderEntryTableHeaderText">Date</th>
+                                <th className="orderEntryTableHeaderText">Order Id</th>
+                                <th className="orderEntryTableHeaderText">Customer Name</th>
+                                <th className="orderEntryTableHeaderText">Order Description</th>
+                                <th className="orderEntryTableHeaderText">Order Status</th>
+                                <th className="orderEntryTableHeaderText">Preview</th>
+                                <th className="orderEntryTableHeaderText">Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr className="orderEntryTableRow">
+                                <td className="orderEntryTableHeaderText">2/20/2022</td>
+                                <td className="orderEntryTableHeaderText">ORD1235</td>
+                                <td className="orderEntryTableHeaderText">New Appeal</td>
+                                <td className="orderEntryTableHeaderText">Jersy</td>
+                                <td className="orderEntryTableHeaderText">
+                                    <div className="orderStatusContainer">
+                                        <div className="circleYellow" />
+                                        <p className="orderStatusText"> 1000 / 200</p>&nbsp;&nbsp;<p className="orderStatusSubText">in kg</p>
+                                    </div>
+                                </td>
+                                <td className="orderEntryTableHeaderText"><Eye size={23} weight="bold" color="#F78D12" /></td>
+                                <td className="orderEntryTableHeaderText"><DotsThreeVertical size={23} weight="bold" color="#F78D12" /></td>
+                            </tr>
+
+                            <tr className="orderEntryTableRow">
+                                <td className="orderEntryTableHeaderText">2/20/2022</td>
+                                <td className="orderEntryTableHeaderText">ORD1235</td>
+                                <td className="orderEntryTableHeaderText">New Appeal</td>
+                                <td className="orderEntryTableHeaderText">Jersy</td>
+                                <td className="orderEntryTableHeaderText">
+                                    <div className="orderStatusContainer">
+                                        <div className="circleGreen" />
+                                        <p className="orderStatusText"> 1000 / 200</p>&nbsp;&nbsp;<p className="orderStatusSubText">in kg</p>
+                                    </div>
+                                </td>
+                                <td className="orderEntryTableHeaderText"><Eye size={23} weight="bold" color="#F78D12" /></td>
+                                <td className="orderEntryTableHeaderText"><DotsThreeVertical size={23} weight="bold" color="#F78D12" /></td>
+                            </tr>
+
+                            <tr className="orderEntryTableRow">
+                                <td className="orderEntryTableHeaderText">2/20/2022</td>
+                                <td className="orderEntryTableHeaderText">ORD1235</td>
+                                <td className="orderEntryTableHeaderText">New Appeal</td>
+                                <td className="orderEntryTableHeaderText">Jersy</td>
+                                <td className="orderEntryTableHeaderText">
+                                    <div className="orderStatusContainer">
+                                        <div className="circleRed" />
+                                        <p className="orderStatusText"> 1000 / 200</p>&nbsp;&nbsp;<p className="orderStatusSubText">in kg</p>
+                                    </div>
+                                </td>
+                                <td className="orderEntryTableHeaderText"><Eye size={23} weight="bold" color="#F78D12" /></td>
+                                <td className="orderEntryTableHeaderText"><DotsThreeVertical size={23} weight="bold" color="#F78D12" /></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="orderEntryFooterContainer">
+
+                    <div className="orderEntryFooterPageSection">
+                        <p className="orderEntryRightText">Page No</p>
+                        <div className="orderEntryPageNumberContainer">
+                            <p className="orderEntryActivePageNumber">1</p>
+                            <p className="orderEntryInactivePageNumber">2</p>
+                            <p className="orderEntryInactivePageNumber">3</p>
+                            <p className="orderEntryInactivePageNumber">4</p>
+                        </div>
+                    </div>
+
+                    <p className="orderEntryRightText">Showing 1 of 4</p>
+                </div>
+
             </div>
+
+            {(showFilterEntry === true) && OrderEntryFilter()}
+
+            {(showAddOrder === true) && <NewOrder setAddOrder={setAddOrder} />}
 
         </div>
     )
